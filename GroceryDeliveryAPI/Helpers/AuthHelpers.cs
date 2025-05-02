8396e504fe9dbf643077d167ed636c5930f4a45e
Thyge123 <95471753+Thyge123@users.noreply.github.com>
2025-05-02 14:00:23 +0200

    8 user registration and authentication (#51)
    * Add user management and authentication features
    
    * temp fix for user creation.
    
    * Refactor JWT token generation and fixed login

--- GroceryDeliveryAPI/Helpers/AuthHelpers.cs
+++ GroceryDeliveryAPI/Helpers/AuthHelpers.cs
@@ -15,26 +15,28 @@ namespace GroceryDeliveryAPI.Helpers
             _configuration = configuration;
         }
 
+
+
         // Generate JWT Token
         public string GenerateJWTToken(User user)
         {
             try
             {
                 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-                    _configuration["Jwt:Key"] ?? throw new InvalidOperationException("JWT key not configured"))); // Get the key from appsettings.json
-                var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256); // Create credentials
-                var claims = new List<Claim> { // Create claims
-                    new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
-                    new Claim(ClaimTypes.Name, user.FirstName + user.LastName),
-                    new Claim(ClaimTypes.Role, user.Role.ToString())
-                };
-                // Create JWT token
+                    _configuration["Jwt:Key"] ?? throw new InvalidOperationException("JWT key not configured")));
+                var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var claims = new List<Claim> {
+            new Claim("id", user.UserId.ToString()),
+            new Claim("name", user.FirstName + " " + user.LastName),
+            new Claim("role", user.Role.ToString())
+        };
+
                 var jwtToken = new JwtSecurityToken(
                     issuer: _configuration["Jwt:Issuer"],
                     audience: _configuration["Jwt:Audience"],
                     claims: claims,
-                    expires: DateTime.Now.AddHours(2), // Token expires in 2 hours
-                    signingCredentials: credentials); // Add credentials
+                    expires: DateTime.Now.AddHours(2),
+                    signingCredentials: credentials);
                 return new JwtSecurityTokenHandler().WriteToken(jwtToken);
             }
             catch (Exception e)
